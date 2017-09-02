71f587216f13f2e57c9bba39dc9b9f69b98714e9
Joshua Harms <joshua@nozzlegear.com>
2017-09-02 14:38:59 -0500

    Use unauthorized module.

--- components/approuter.tsx
+++ components/approuter.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import AutoProp from 'auto-prop-component';
 import Paths from '../modules/paths';
 import { Auth } from '../stores';
+import { handleUnauthorized } from '../modules/unauthorized';
 import { History } from 'history';
 
 export abstract class AppRouter<IProps, IState> extends AutoProp<IProps, IState> {
@@ -22,20 +23,7 @@ export abstract class AppRouter<IProps, IState> extends AutoProp<IProps, IState>
 
     public PATHS = Paths;
 
-    /**
-     * Notifies the user that their account has been logged out, and that they must log in again before their request can be made. Returns true if they accept the prompt to log in again, false if not.
-     */
-    public handleUnauthorized(redirectBackTo?: string, querystring?: Object) {
-        if (confirm("Your account has been logged out, and you must log in again before your request can be made. Do you want to log in again?")) {
-            window.location.search = qs.stringify({ redirect: redirectBackTo, qs: querystring });
-
-            Auth.logout();
-
-            return true;
-        }
-
-        return false;
-    }
+    public handleUnauthorized = handleUnauthorized
 }
 
 export default AppRouter;
